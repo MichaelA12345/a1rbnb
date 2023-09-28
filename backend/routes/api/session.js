94fb13e3d96c9156d450fdb82e0f76e5b532bc05
Michael A <michaelaramde@gmail.com>
2023-09-28 16:23:42 -0700

    Add User logout backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -44,5 +44,11 @@ router.post(
       });
     }
   );
-
+router.delete(
+  '/',
+  (_req, res) => {
+    res.clearCookie('token');
+    return res.json({ message: 'success' });
+  }
+);
 module.exports = router;
