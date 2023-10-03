fc11a3929a439ed87b4daeba9621973a147b9af2
Michael A <michaelaramde@gmail.com>
2023-10-03 10:37:40 -0700

    Added seeders for join tables

---
+++ backend/db/seeders/20231003173338-demo-spot-images.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const { SpotImage } = require('../models');
+const bcrypt = require("bcryptjs");
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await SpotImage.bulkCreate([
+      {
+        spotId: 1,
+        url: 'localhost:9001',
+        preview: false,
+        
+      }
+    ], { validate: false });
+  },
+
+  async down (queryInterface, Sequelize) {
+    options.tableName = 'SpotImages';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      spotId: { [Op.in]: [1, 2] }
+    }, {});
+  }
+};
